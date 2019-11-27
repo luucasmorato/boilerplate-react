81b51e1dd25a3c6c2b2feefb4e80daf540d34f0b
lucas_morato <lucas.morato@touchcomp.com.br>
2019-11-27 17:04:05 -0300

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './styles/global';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <Home />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
